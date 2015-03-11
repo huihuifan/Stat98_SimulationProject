2da9d10478ad6e88a93e62bed15430880ebe29d6
Matteo Bonvini <matteobonvini@college.harvard.edu>
2015-03-11 02:24:08 -0400

    Add multiple impuation fun

--- Code/Stat98_Simulation.R
+++ Code/Stat98_Simulation.R
@@ -1,9 +1,11 @@
 ####################
 # Import libraries #
 ####################
-
+setwd( "C:/Users/Matteo/Desktop/Stat98_SimulationProject/Code")
 library(truncnorm)
 library(mice)
+library("mi")
+library("calibrate")
 
 source("helper_functions.R")
 
@@ -13,7 +15,7 @@ source("helper_functions.R")
 ###############################
 
 # coefficients used to generate missingness
-coeff.miss <- c(1, 1, 1)
+coeff.miss <- c(3, 3, 3)
 
 # missing probability
 prob <- c(.05, .15, .25)
@@ -44,6 +46,48 @@ run_simulation(num_iters=1000, missing_method="MNAR", coeff.miss=coeff.miss, pro
 run_simulation(num_iters=1000, missing_method="MNAR", coeff.miss=coeff.miss, prob[3], 
                impute_method="complete", level=.9, true_betas=c(10, .7, .8))
 
+run_simulation(num_iters=1000, missing_method="MCAR", coeff.miss=coeff.miss, prob[3], 
+               impute_method="single", level=.9, true_betas=c(10, .7, .8))
+run_simulation(num_iters=1000, missing_method="MCAR", coeff.miss=coeff.miss, prob[2], 
+               impute_method="single", level=.9, true_betas=c(10, .7, .8))
+run_simulation(num_iters=1000, missing_method="MCAR", coeff.miss=coeff.miss, prob[3], 
+               impute_method="single", level=.9, true_betas=c(10, .7, .8))
+
+run_simulation(num_iters=1000, missing_method="MAR", coeff.miss=coeff.miss, prob[3], 
+               impute_method="single", level=.9, true_betas=c(10, .7, .8))
+run_simulation(num_iters=1000, missing_method="MAR", coeff.miss=coeff.miss, prob[2], 
+               impute_method="single", level=.9, true_betas=c(10, .7, .8))
+run_simulation(num_iters=1000, missing_method="MAR", coeff.miss=coeff.miss, prob[3], 
+               impute_method="single", level=.9, true_betas=c(10, .7, .8))
+
+run_simulation(num_iters=1000, missing_method="MNAR", coeff.miss=coeff.miss, prob[3], 
+               impute_method="single", level=.9, true_betas=c(10, .7, .8))
+run_simulation(num_iters=1000, missing_method="MNAR", coeff.miss=coeff.miss, prob[2], 
+               impute_method="single", level=.9, true_betas=c(10, .7, .8))
+run_simulation(num_iters=1000, missing_method="MNAR", coeff.miss=coeff.miss, prob[3], 
+               impute_method="single", level=.9, true_betas=c(10, .7, .8))
+
+run_simulation(num_iters=1000, missing_method="MCAR", coeff.miss=coeff.miss, prob[3], 
+               impute_method="multiple", level=.9, true_betas=c(10, .7, .8))
+run_simulation(num_iters=1000, missing_method="MCAR", coeff.miss=coeff.miss, prob[2], 
+               impute_method="multiple", level=.9, true_betas=c(10, .7, .8))
+run_simulation(num_iters=1000, missing_method="MCAR", coeff.miss=coeff.miss, prob[3], 
+               impute_method="multiple", level=.9, true_betas=c(10, .7, .8))
+
+run_simulation(num_iters=1000, missing_method="MAR", coeff.miss=coeff.miss, prob[3], 
+               impute_method="multiple", level=.9, true_betas=c(10, .7, .8))
+run_simulation(num_iters=1000, missing_method="MAR", coeff.miss=coeff.miss, prob[2], 
+               impute_method="multiple", level=.9, true_betas=c(10, .7, .8))
+run_simulation(num_iters=1000, missing_method="MAR", coeff.miss=coeff.miss, prob[3], 
+               impute_method="multiple", level=.9, true_betas=c(10, .7, .8))
+
+run_simulation(num_iters=1000, missing_method="MNAR", coeff.miss=coeff.miss, prob[3], 
+               impute_method="multiple", level=.9, true_betas=c(10, .7, .8))
+run_simulation(num_iters=1000, missing_method="MNAR", coeff.miss=coeff.miss, prob[2], 
+               impute_method="multiple", level=.9, true_betas=c(10, .7, .8))
+run_simulation(num_iters=1000, missing_method="MNAR", coeff.miss=coeff.miss, prob[3], 
+               impute_method="multiple", level=.9, true_betas=c(10, .7, .8))
+
 
 dat <- gen_data()
 d.w.m <- genMNAR(df = dat, prop = c(.75, .75, .75), beta.missing = c(1, 1, 1), 
@@ -56,9 +100,25 @@ confint(fit, "edu") # just stops working at a point
 
 
 # METHOD 3: Multiple Imputation
-library("mice")
-library("mi")
-library("calibrate")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 par(mfrow = c(1, 1))
 
 head(data)
